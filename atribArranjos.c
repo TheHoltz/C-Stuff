ce2eff9b5c996460dcbdd43b42e8a38e701d06d3
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-10-20 18:22:51 -0300

    Add files via upload

---
+++ atribArranjos.c
@@ -0,0 +1,16 @@
+//este arquivo contém um exemplo de atribuição de arranjos como estruturas
+//além de um exemplo de ponteiro para função
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	struct lampiao{int a1[2];}; //definindo uma estrutura arbitrária
+	struct lampiao k={{1,2}}; //defindo uma variável da estrutura anterior e a inicializando
+	struct lampiao n; //definindo uma segunda variável da mesma estrutura porem sem inicializacao
+	n=k; //atribuindo os valores da primeira a variável gerada anteriormente.
+	printf("Valor: %d\n", n.a1[0]);
+	return 0;
+}
